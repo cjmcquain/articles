4fde342a38e02572f1d4cbcc65def6136f99e452
Charles McQuain <charlesmcquain@gmail.com>
2018-03-03 20:55:30 -0800

    Added 'Add Article' page

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const exphbs  = require('express-handlebars');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
 
 app.get('/', (req, res) => {
-    res.send('Test');
+    res.render('home');
+});
+
+app.get('/articles/add', (req, res) => {
+    res.render('articles/add');
 });
 
 app.listen(3000, () => {
